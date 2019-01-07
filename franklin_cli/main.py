e86c73a55791313cd90a8c8fcde37b00f5eca7ba
Tristanux <tristanux@gmail.com>
2019-01-07 16:25:35 +0100

    Simple cli version

--- franklin_cli/main.py
+++ franklin_cli/main.py
@@ -7,23 +7,18 @@ import os
 import sys
 import argparse
 import logging
-from subprocess import run, Popen, PIPE
+import subprocess
 from helpers import load_yaml_config, logger
 import pexpect
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Franklin CLI")
-    parser.add_argument("--tub", required=True,
-                        help="Name of the exported tub")
-    parser.add_argument("--pilot", required=True,
-                        help="Name of the exported pilot")
-    parser.add_argument("--config", default="conf/default.yaml",
-                        help="Path to config file : conf/my_config.yaml")
-    parser.add_argument("--local_tub", help="""Use local tub on your local machine.
+    parser.add_argument("--local_tub", help="""Path to local tub on your local machine.""")
+    parser.add_argument("--user", help="""The username for tensorflow-gpu (i.e. tristan_letrou_ysance_com).
     Needs path to use a tub on this machine.""")
 
     args = parser.parse_args()
-    config_path, tub, pilot, localSource = args.config, args.tub, args.pilot, args.local_tub
+    config_path, local_tub, user = args.config, args.local_tub, args.user
 
     logger = logger()
     logger.info("Start training!")
@@ -32,28 +27,33 @@ if __name__ == "__main__":
     absolute_config_path = os.path.join(os.path.dirname(__file__), config_path)
     config = load_yaml_config(absolute_config_path)
 
-    # Get the tub with ssh with rsync
-    if localSource is None:
-        rsync_cmd = "rsync -r {}@{}:{} {}".format(config['machine']['user'], config['machine']['address'], config['machine']['tub_path'], "~/" + tub)                               
-        print(rsync_cmd)                   
-        child = pexpect.spawn(rsync_cmd)
-        # child.logfile = sys.stdout
-        child.expect('password:')
-        child.sendline(config['machine']['password'])
+    if(user is None):
+        if("user" not in config):
+            print('Please choose a user, either in conf or as --user flag')
+            sys.exit(1)
+        user = config.user
 
-        print('cvzavz')
-        # result = run(rsync_cmd.split(" "))
-        # print(config['machine']['password'], file=PIPE)
+    clean_working_tub = "rm -rf tub && rm -f tub.zip"
+    subprocess.call(clean_working_tub, shell=True)
 
-        # with Popen(rsync_cmd, stdin=PIPE, stdout=PIPE,
-        #            universal_newlines=True) as p:
-        #     for line in p.stdout:
-        #         if line.startswith("Please enter the name of the project"):
-        #         print(answer, file=p.stdin)  # Answer password
-        #         p.stdin.flush()
+    if(local_tub is None):
+        get_tub_cmd = "sshpass -p raspberry rsync -r pi@192.168.43.200:~/mycar/tub ."
+    else:
+        get_tub_cmd = "cp -r {} .".format(local_tub)
+    subprocess.call(get_tub_cmd, shell=True)
 
-        # call(rsync_cmd)
+    cmd = """zip -r tub.zip tub &&\
+            gcloud compute scp --project dmp-y-tests --zone europe-west1-d  tub.zip {user}@tensorflow-gpu:~/mycar &&\
+            gcloud compute ssh --project dmp-y-tests --zone europe-west1-d {user}@tensorflow-gpu \
+            -- 'source env/bin/activate &&\
+                cd ~/mycar &&\
+                rm -rf tub && rm -f mypilot &&\
+                unzip tub &&\
+                python manage.py train --tub tub --model mypilot' &&\
+            gcloud compute scp --project dmp-y-tests --zone europe-west1-d {user}@tensorflow-gpu:~/mycar/mypilot . &&\
+            sshpass -p raspberry rsync mypilot pi@192.168.43.200:~/mycar/mypilot
+    """.format(user=user)
 
-    logger.info("Not yet implemented")
+    subprocess.call(cmd, shell=True)
 
     logger.info("Finished!")
