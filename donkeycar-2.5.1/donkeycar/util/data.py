e075580b931b3c41ba26940f513cf206190b3eeb
baptiste.poirier <baptiste.poirier@ysance.com>
2019-01-07 18:34:12 +0100

    Added a print in data/map_range

--- donkeycar-2.5.1/donkeycar/util/data.py
+++ donkeycar-2.5.1/donkeycar/util/data.py
@@ -81,7 +81,7 @@ def map_range(x, X_min, X_max, Y_min, Y_max):
     X_range = X_max - X_min
     Y_range = Y_max - Y_min
     XY_ratio = X_range / Y_range
-
+    print(x,X_range,Y_range,XY_ratio)
     y = ((x - X_min) / XY_ratio + Y_min) // 1
 
     return int(y)
