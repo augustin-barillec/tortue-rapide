d1c98cea23dc3dff175b8e9a4918aaeedc9a3ab9
baptiste.poirier <baptiste.poirier@ysance.com>
2019-01-07 18:25:56 +0100

    Fix issues with predict

--- donkeycar-2.5.1/donkeycar/parts/keras.py
+++ donkeycar-2.5.1/donkeycar/parts/keras.py
@@ -72,9 +72,9 @@ class KerasCategorical(KerasPilot):
 
     def run(self, img_arr):
         img_arr = img_arr.reshape((1,) + img_arr.shape)
-        angle_binned, throttle = self.model.predict(img_arr)
+        angle_binned = self.model.predict(img_arr)
         angle_unbinned = util.data.linear_unbin(angle_binned[0])
-        return angle_unbinned, throttle[0][0]
+        return angle_unbinned, 0
 
 
 class KerasLinear(KerasPilot):
