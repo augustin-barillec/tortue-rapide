f7ad67297320693691c8b3094a1fd5c3e3342e96
raphael <rca@ysance.com>
2019-03-04 19:56:05 +0100

    add augmentation methods and script training

--- franklin_gym/utils.py
+++ franklin_gym/utils.py
@@ -18,7 +18,28 @@ def linear_bin(a, n_class):
     Returns
     -------
     list of int
-        A list of length 15 with one item set to 1, which represents the linear value, and all other items set to 0.
+        A list of length n_class with one item set to 1, which represents the linear value, and all other items set to 0.
+    """
+    a = a + 1
+    b = round((n_class - 1) * a / 2)
+    arr = np.zeros(n_class)
+    arr[int(b)] = 1
+    return arr
+
+def linear_bin(a, n_class):
+    """
+    Convert categorical array to a continuous value.
+
+    Parameters
+    ----------
+    list of int
+        A list of length n_class with one item set to 1, which represents the linear value, and all other items set to 0.
+
+    Returns
+    -------
+    float
+        value between -1 and 1
+
     """
     a = a + 1
     b = round((n_class - 1) * a / 2)
@@ -56,13 +77,38 @@ def tub_to_array(tub_path, n_class, n_first_files=None):
             y_angle.append(record['user/angle'])
             y_throttle.append(record['user/throttle'])
 
-    y = {'angle_float': np.asarray(y_angle),
-         'throttle_out': np.asarray(y_throttle)}
+    # y = {'angle_float': np.asarray(y_angle),
+    #      'throttle_out': np.asarray(y_throttle)}
 
-    res = []
-    for n in y['angle_float']:
-        res.append(linear_bin(n, n_class))
+    angle_categorical = []
+    for n in y_angle:
+        angle_categorical.append(linear_bin(n, n_class))
 
-    y['angle_cat_out'] = np.asarray(res)
+    # y['angle_cat_out'] = np.asarray(angle_categorical)
 
-    return x, y
+    return x, np.asarray(angle_categorical)
+
+
+
+def rebalance(X, Y, replace=False):
+    """
+    Rebalance classes using undersampling on the over represented classes
+
+    X : numpy array image
+    Y : must by numpy array of length 3 eg [1, 0, 0]
+    """
+    left = np.array([np.array_equal(a, np.array([1, 0, 0])) for a in Y])
+    center = np.array([np.array_equal(a, np.array([0, 1, 0])) for a in Y])
+    right = np.array([np.array_equal(a, np.array([0, 0, 1])) for a in Y])
+
+    index = np.array(range(len(Y)))
+    n_wings = min(sum(left), sum(right))
+    n_center = int(n_wings * 1.4) # 1.4 from observed data
+
+    center_classes = np.random.choice(index[center], size=n_center, replace=replace)
+    right_classes = np.random.choice(index[right], size=n_wings, replace=replace)
+    left_classes = np.random.choice(index[left], size=n_wings, replace=replace)
+
+    to_keep = np.concatenate((center_classes, right_classes, left_classes))
+
+    return X[to_keep], Y[to_keep]
